2c4c02fe4b4ad4a690d1c7db6b32a0703653e478
Jerome <jerome@levelup-agency.com>
2026-01-09 00:20:54 +0100

    feat: add theme provider with persistence

---
+++ app/lib/core/providers/theme_provider.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+const _themeModeKey = 'theme_mode';
+
+final sharedPreferencesProvider = Provider<SharedPreferences>((ref) {
+  throw UnimplementedError('SharedPreferences must be overridden in main');
+});
+
+final themeModeProvider = StateNotifierProvider<ThemeModeNotifier, ThemeMode>((ref) {
+  final prefs = ref.watch(sharedPreferencesProvider);
+  return ThemeModeNotifier(prefs);
+});
+
+class ThemeModeNotifier extends StateNotifier<ThemeMode> {
+  final SharedPreferences _prefs;
+
+  ThemeModeNotifier(this._prefs) : super(_loadThemeMode(_prefs));
+
+  static ThemeMode _loadThemeMode(SharedPreferences prefs) {
+    final value = prefs.getString(_themeModeKey);
+    switch (value) {
+      case 'light':
+        return ThemeMode.light;
+      case 'dark':
+        return ThemeMode.dark;
+      default:
+        return ThemeMode.system;
+    }
+  }
+
+  Future<void> setThemeMode(ThemeMode mode) async {
+    state = mode;
+    await _prefs.setString(_themeModeKey, mode.name);
+  }
+}
